2228533f90ef4fae0e992a4d186ee44a35a8abb1
Noah Pflugradt <Noah.Pflugradt@gmail.com>
2022-01-26 12:58:56 +0100

    first public release

---
+++ examples/visualizer.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+def plotdf(myseries, filename):
+    ax = myseries.plot(figsize=(32, 20))
+    fig = ax.get_figure()
+    fig.savefig(filename)
+    fig.clear()
+
+df1 = pd.read_csv("summed_up_t1.csv", header=None)
+df1.columns = ['idx','val']
+df_r = df1.copy()
+plotdf(df_r['val'].rolling(365 * 24).median(), "myplot.png")
+dayoffset = 31+29+31+5
+dfjan = df1[1440*dayoffset:1440*(dayoffset+7)]['val']
+
+plotdf(dfjan, "myplotjan.png")
